b6d28e43945467902c9f428cc2d485b084cd9e2d
Razak <mohammedrazak2001@gmail.com>
2021-11-26 20:51:43 +0530

    restructure

--- Ecommerce_App/settings.py
+++ Ecommerce_App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vw)@41nli-dq0s-_fhh&=%i7krobozm9dwja%==8+o&&bi9tcj'
+SECRET_KEY = 'django-insecure-5=_=yar1j^+uuazb=*zb^=svp#!gpf(^ms5q5@h=paifbg(=79'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
