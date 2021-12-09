80080dcb6f5d58a85549cf1e1ee982f42db1f83f
Nachiketa Nalin <nalinnachiketa@gmail.com>
2021-12-10 02:34:02 +0530

    login signup added

--- Ecommerce_App/settings.py
+++ Ecommerce_App/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'Ecommerce_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': 'nachiketa',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
